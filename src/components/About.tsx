66fe18c7dab1fce35fc7fa026782e2320b6e6704
Morsadi <badreddine.morsadi@github.com>
2020-10-27 15:19:11 -0700

    About Page

---
+++ src/components/About.tsx
@@ -0,0 +1,51 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+import { motion } from 'framer-motion';
+
+export default function About() {
+  const pageVariants = {
+    initial: {
+      opacity: 0,
+    },
+    in: {
+      opacity: 1,
+    },
+    out: {
+      opacity: 0,
+    },
+  };
+
+  const signupPath  = ()=>{
+    window.location.href = '/signup';
+  }
+  return (
+    <motion.div
+      initial='initial'
+      animate='in'
+      exit='out'
+      variants={pageVariants}
+      transition={{ duration: 1 }}
+      className='page about'
+      style={{ height: window.innerHeight }}
+    >
+      <h2>Mike's Group</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut impedit
+        omnis quos commodi eaque accusamus odit ratione officia consectetur a,
+        eveniet neque asperiores expedita est veniam quo minus velit
+        consequuntur. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ut impedit omnis quos commodi eaque accusamus odit ratione officia
+        consectetur a, eveniet neque asperiores expedita est veniam quo minus
+        velit consequuntur.
+      </p>
+
+      <input className='join' type='button' value='JOIN' />
+      <input
+        onClick={signupPath}
+        className='play'
+        type='button'
+        value='PLAY'
+      />
+    </motion.div>
+  );
+}
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -18,6 +18,13 @@ export default function NavBar({ navScroll }: any) {
               href='/'
               style={{ opacity: location !== '/' ? '.4' : '1' }}
             >
+              ABOUT
+            </Nav.Link>
+            <Nav.Link
+              className='NavLink'
+              href='/signup'
+              style={{ opacity: location !== '/signup' ? '.4' : '1' }}
+            >
               SIGNUP
             </Nav.Link>
             <Nav.Link
