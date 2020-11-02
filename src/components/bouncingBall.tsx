ad3f7b68e1d12fd77a2a72a1f4525059c24c3936
Morsadi <badreddine.morsadi@github.com>
2020-11-02 15:00:35 -0700

    Bouncing ball update

--- src/components/bouncingBall.tsx
+++ src/components/bouncingBall.tsx
@@ -2,33 +2,45 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const ballStyle = {
-  background: '#fff',
-  height: '10px',
-  width: '10px',
-  borderRadius: '50%',
-  opacity:.6,
-};
-
-const ballTransition = {
-  y: {
-    duration: 0.6,
-    yoyo: Infinity,
-    ease: 'easeOut',
-  },
-  x: {
-    duration: 1.5,
-    yoyo: Infinity,
-    ease: 'linear',
-  },
-};
 export default function Bouncy() {
+  const ballStyle = {
+    background: '#fff',
+    height: '20px',
+    width: '20px',
+    borderRadius: '50%',
+    opacity: 0.6,
+  };
+
+  const ballTransition = {
+    y: {
+      duration: 1,
+      yoyo: Infinity,
+      ease: 'easeOut',
+    },
+    x: {
+      duration: 2.2,
+      yoyo: Infinity,
+      ease: 'linear',
+    },
+  };
+  const mobile = () => {
+    return window.innerWidth <= 768;
+  };
   return (
-    <div id='bouncyBall' style={{ position: 'absolute', left : '0', bottom: '240px', textAlign:'center'}}>
+    <div
+      id='bouncyBall'
+      style={{
+        position: 'absolute',
+        left: '0',
+        bottom: '240px',
+        textAlign: 'center',
+        width: '100%',
+      }}
+    >
       <motion.div
         animate={{
-          y: ['400%', '-600%'],
-          x: window.innerWidth,
+          y: ['400%', `${mobile() ? '-200%' : '-400%'}`],
+          x: '100vw',
         }}
         transition={ballTransition}
         style={ballStyle}
